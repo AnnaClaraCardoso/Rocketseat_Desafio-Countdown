15adc0fdfa031293fc483cadc31f63cb775433cb
AnnaClaraCardoso <84738118+AnnaClaraCardoso@users.noreply.github.com>
2022-04-04 15:53:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,37 +6,35 @@ const DEFAULT_TIME_LABELS = {
 };
 
 class Countdown {
-  constructor(root, days, hours, minutes, seconds) {
-    root.innerHTML = Countdown.getHTML()
+  constructor(days, hours, minutes, seconds) {
 
-    this.remainingSeconds = 0
     this.interval = null
 
+    this.counterHTML = `
+      <span class="countdown-element days"></span>
+      <span class="countdown-element">:</span>
+      <span class="countdown-element hours"></span>
+      <span class="countdown-element">:</span>
+      <span class="countdown-element minutes"></span>
+      <span class="countdown-element">:</span>
+      <span class="countdown-element seconds"></span>
+    `
+
+    this.remainingSeconds = 0
+
     this.element = {
-      days: root.querySelector('.days'),
-      hours: root.querySelector('.hours'),
-      minutes: root.querySelector('.minutes'),
-      seconds: root.querySelector('.seconds'),
+      days: counterHTML.querySelector('.days'),
+      hours: counterHTML.querySelector('.hours'),
+      minutes: counterHTML.querySelector('.minutes'),
+      seconds: counterHTML.querySelector('.seconds'),
     }
     this.remainingSeconds = ((days*86400)+(hours*3600)+(minutes*60)+seconds)
-  }
-
-  updateInterfaceTime() {
-    const days = Math.floor(this.remainingSeconds / 86400);
-    const hours = Math.floor(this.remainingSeconds / 3600);
-    const minutes = Math.floor(this.remainingSeconds / 60);
-    const seconds = this.remainingSeconds % 60;
-    
-    this.element.days.textContent = days.toString().padStart(2, "0");
-    this.element.hours.textContent = hours.toString().padStart(2, "0");
-    this.element.minutes.textContent = minutes.toString().padStart(2, "0");
-    this.element.seconds.textContent = seconds.toString().padStart(2, "0");
   }
 
   start() {
     if (this.remainingSeconds === 0) return;
 
-    this.interval = setInterval(() => {
+    this.interval = setIntervalpre(() => {
       this.remainingSeconds--;
       this.updateInterfaceTime();
 
@@ -52,20 +50,18 @@ class Countdown {
     this.interval = null;
   }
 
-  static getHTML() {
-    return `
-    <span class="countdown-element days"></span>
-    <span class="countdown-element">:</span>
-    <span class="countdown-element hours"></span>
-    <span class="countdown-element">:</span>
-    <span class="countdown-element minutes"></span>
-    <span class="countdown-element">:</span>
-    <span class="countdown-element seconds"></span>
-		`;
+  updateInterfaceTime() {
+    const days = Math.floor(this.remainingSeconds / 86400);
+    const hours = Math.floor(this.remainingSeconds / 3600);
+    const minutes = Math.floor(this.remainingSeconds / 60);
+    const seconds = this.remainingSeconds % 60;
+    
+    this.element.days.textContent = days.toString().padStart(2, "0");
+    this.element.hours.textContent = hours.toString().padStart(2, "0");
+    this.element.minutes.textContent = minutes.toString().padStart(2, "0");
+    this.element.seconds.textContent = seconds.toString().padStart(2, "0");
   }
 
 }
-  
-new Countdown(
-	document.querySelector(".countdown", 0, 0, 3, 30)
-);
+
+document.addEventListener("load", new Countdown(0, 0, 1, 30))
